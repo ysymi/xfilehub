1d533f7de1e821f9dd04fdb3364f2f5790504368
ysymi <ff.2011@live.cn>
2016-05-24 10:07:57 +0800

    re arange the code

--- config.py
+++ config.py
@@ -1,20 +1,15 @@
 import os
 
-ROOT = os.path.dirname(__file__)
-STORAGE_DIR = os.path.join(ROOT, 'storage')
-BLOCKS_FILE = os.path.join(ROOT, 'block', 'block.index')
-
 PORT = 9000
 BUFFER_SIZE = 1024 * 1024
 
-LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
-
-CHUNK_NOTE_PATH = os.path.join(STORAGE_DIR, 'chunk.note')
+ROOT = os.path.dirname(__file__)
+STORAGE_DIR = os.path.join(ROOT, 'storage')
+FILE_NOTE_PATH = os.path.join(STORAGE_DIR, 'files.note')
 GROUPS_NOTE_PATH = os.path.join(STORAGE_DIR, 'groups.note')
 
-STORAGE_PORTS = list(range(8000, 8010))
 STORAGE_HOST = 'localhost'
+STORAGE_PORTS = list(range(8000, 8010))
 
-
-FILE_MAP_PATH = os.path.join(STORAGE_DIR, 'files')
-CHUNK_NAME_FORMAT = '%{name}s.#%{seq}s'
+LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+CHUNK_NAME_FORMAT = '%(name)s.#%(seq)s'

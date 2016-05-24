2396cd62e2ba12530cfd99367921ef86f2d57f5c
ysymi <ff.2011@live.cn>
2016-05-24 12:45:19 +0800

    add log

--- handler/main.py
+++ handler/main.py
@@ -1,6 +1,9 @@
+import logging
+
 from tornado.web import RequestHandler
 
 
 class MainHandler(RequestHandler):
     def get(self):
+        logging.info('/ is called redirect now')
         self.redirect('/files')

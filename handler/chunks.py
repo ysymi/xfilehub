0ae10a719b97a52446dbe75381c7cf8070971a41
ysymi <ff.2011@live.cn>
2016-05-24 10:12:45 +0800

    mv file post in chunks

--- handler/chunks.py
+++ handler/chunks.py
@@ -1,3 +1,5 @@
+import logging
+
 from tornado.web import RequestHandler
 
 from config import CHUNK_NAME_FORMAT
@@ -9,11 +11,14 @@ class ChunksHandler(RequestHandler):
     def post(self):
         name = self.get_body_argument('name')
         seq = self.get_body_argument('seq')
-        chunk_name = CHUNK_NAME_FORMAT % (name, seq)
+        chunk_name = CHUNK_NAME_FORMAT.format(name=name, seq=seq)
 
+        logging.info('get chunk: %s' % chunk_name)
         group_name = hashring.find(chunk_name)
         group = groups.get(group_name)
 
-        upload_url = 'http://%{host}s:%{port}/upload' % (group.host, group.port)
+        logging.info('will save to %s' % group_name)
 
+        upload_url = 'http://{host}:{port}/upload'.format(host=group['host'], port=group['port'])
+        logging.info(upload_url)
         return self.redirect(upload_url, status=307)
--- handler/file.py
+++ handler/file.py
@@ -1,26 +1,14 @@
+import logging
+
 from tornado.web import RequestHandler
 
-from config import CHUNK_NAME_FORMAT
 from storage.files import files
-from storage.groups import groups
-from storage.hashring import hashring
 from util.request import do_request
 
 
 class FileHandler(RequestHandler):
-    def post(self):
-        name = self.get_body_argument('name')
-        seq = self.get_body_argument('seq')
-        chunk_name = CHUNK_NAME_FORMAT % (name, seq)
-
-        group_name = hashring.find(chunk_name)
-        group = groups.get(group_name)
-
-        upload_url = 'http://%{host}s:%{port}/upload' % (group.host, group.port)
-
-        return self.redirect(upload_url, status=307)
-
     def get(self, filename):
+        logging.info('recive %s ' % filename)
         # return self.redirect('http://127.0.0.1:9000/download/%s' % filename, status=307)
 
         self.set_header('Content-Type', 'application/octet-stream')

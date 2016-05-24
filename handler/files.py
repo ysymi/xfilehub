4c451843239aa456c7c17364e884869ccf2904ba
ysymi <ff.2011@live.cn>
2016-05-24 15:17:31 +0800

    add log

--- handler/files.py
+++ handler/files.py
@@ -1,9 +1,11 @@
 from tornado.web import RequestHandler
 
 from storage.files import files
+import logging
 
 
 class FilesHandler(RequestHandler):
     def get(self):
+        logging.info('/files called')
         file_list = files.get_files()
         self.render('index.html', file_list=file_list)

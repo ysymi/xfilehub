ccc6a5fb66555462e54334eb92fa0ba3492407f8
ysymi <ff.2011@live.cn>
2016-06-01 16:01:25 +0800

    add collect

--- storage/groups.py
+++ storage/groups.py
@@ -9,67 +9,64 @@ from util.util import port_is_used
 
 class Groups(object):
     def __init__(self):
-        self._groups = []
+        self._groups = {}
         self.recovery()
         if not self._groups or not self.check():
             self.rebuild()
-            self.save()
 
     def recovery(self):
         if not os.path.exists(GROUPS_NOTE_PATH):
             logging.info('group.note is not exist')
             return
+
         with open(GROUPS_NOTE_PATH, 'r') as f:
             self._groups = json.loads(f.read())
 
     def check(self):  # TODO reduce replica
+        if len(self._groups) == len(STORAGE_PORTS):
+            return True
+
         logging.info('check groups now')
-        online_groups = []
-        for port in STORAGE_PORTS:
-            if port_is_used(port):
-                info = do_request('/info', STORAGE_HOST, port, to_dict=True)
-                group = {
-                    'name': info['name'],
-                    'host': info['host'],
-                    'port': info['port'],
-                    # 'master': info.masters,
-                    # 'slaves': info.slaves
-                }
-                online_groups.append(group)
-        logging.info('online groups : %s  self.grups: %s' % (len(online_groups), len(self._groups)))
-        return len(self._groups) == len(online_groups)
+        # online_groups = self.collect()
+        # logging.info('online groups : %s  self.grups: %s' % (len(online_groups), len(self._groups)))
+        return len(self._groups) == len(self.collect())
 
     def rebuild(self):
-        self._groups = []
-        for port in STORAGE_PORTS:
-            if port_is_used(port):
-                info = do_request('/info', STORAGE_HOST, port, to_dict=True)  # TODO finish /info
-                group = {
-                    'name': info['name'],
-                    'host': info['host'],
-                    'port': info['port'],
-                    # 'master': info.masters,
-                    # 'slaves': info.slaves
-                }
-                self._groups.append(group)
+        self._groups = self.collect()
         self.save()
 
     def save(self):
         with open(GROUPS_NOTE_PATH, 'w') as f:
-            f.write(json.dumps(self._groups, indent=2))
+            f.write(json.dumps(self._groups, indent=2, sort_keys=True))
+
+    @staticmethod
+    def collect():
+        online_groups = {}
+        for port in STORAGE_PORTS:
+            if port_is_used(port):
+                try:
+                    info = do_request('/info', STORAGE_HOST, port, to_dict=True)  # TODO finish /info
+                    group = {
+                        'host': info['host'],
+                        'port': info['port']
+                    }
+                    online_groups[info['name']] = group
+                except Exception as e:
+                    logging.exception('can not get info of %s, %s' % (port, str(e)))
+
+        return online_groups
 
     # TODO: check this is used
-    def insert(self, group_name, md5):
-        self._groups.append({
-            'name': group_name,
-            'md5': md5,
-        })
-        self.save()
+    # def insert(self, group_name, md5):
+    #     self._groups.append({
+    #         'name': group_name,
+    #         'md5': md5,
+    #     })
+    #     self.save()
 
     def get(self, name):
-        for group in self._groups:
-            if group['name'] == name:
-                return group
+        if name in self._groups.keys():
+            return self._groups[name]
         return None
 
     def get_all(self):
@@ -77,7 +74,6 @@ class Groups(object):
         if len(self._groups) == 0 or not self.check():
             logging.info('will rebuild groups')
             self.rebuild()
-            self.save()
         return self._groups
 
 
